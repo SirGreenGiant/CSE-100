cf698c8fc785fc1170403941a8b906425a6b0e68
SirGreenGiant <brendans0901@gmail.com>
2026-08-28 15:24:52 -0600

    Update Lab1_Q2.cpp
    I hate autograders

--- Labs/Lab_1/Lab1_Q2.cpp
+++ Labs/Lab_1/Lab1_Q2.cpp
@@ -8,12 +8,12 @@ int main()
     cout << "******************************" << endl;
     cout << endl;
 
-    cout << "1. The sum of 2 and 3 \t \t \t= " << 2 + 3  << endl;
-    cout << "2. The product of 5 and 6 \t \t= " << 5 * 6  << endl;
-    cout << "3. 15 divided by 7 gives quotient\t = " << 15 / 7 << endl;
-    cout << "4. 15 divided by 4 has remainder\t = " << 15 / 4 << endl;
+    cout << "1. The sum of 2 and 3\t\t\t= " << 2 + 3  << endl;
+    cout << "2. The product of 5 and 6\t\t= " << 5 * 6  << endl;
+    cout << "3. 15 divided by 7 gives quotient\t= " << 15 / 7 << endl;
+    cout << "4. 15 divided by 4 has remainder\t= " << 15 / 4 << endl;
     cout << endl;
-    cout << "\tThis is the end of my first Program" << endl;
+    cout << "\tThis is end of my first Program" << endl;
     cout << "\t \t \t \t \t Thank you!" << endl;    
     return 0;
 }
