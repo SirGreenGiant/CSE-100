c66fd5be0a01464a04f912b79c67b5d21cc89a42
SirGreenGiant <brendans0901@gmail.com>
2026-08-28 15:19:30 -0600

    Update Lab1_Q2.cpp

--- Labs/Lab_1/Lab1_Q2.cpp
+++ Labs/Lab_1/Lab1_Q2.cpp
@@ -3,17 +3,17 @@ using namespace std;
 int main()
 {
     cout << "******************************" << endl;
-    cout << "\t \t CSE 100" << endl;
+    cout << "\tCSE 100" << endl;
     cout << " Welcome to C++ Programming" << endl;
     cout << "******************************" << endl;
     cout << endl;
 
-    cout << "1. The sum of 2 and 3 \t \t \t \t = " << 2 + 3  << endl;
-    cout << "2. The product of 5 and 6 \t \t \t = " << 5 * 6  << endl;
-    cout << "3. 15 divided by 7 gives quotient \t = " << 15 / 7 << endl;
-    cout << "4. 15 divided by 4 has remainder \t = " << 15 / 4 << endl;
+    cout << "1. The sum of 2 and 3 \t \t \t= " << 2 + 3  << endl;
+    cout << "2. The product of 5 and 6 \t \t= " << 5 * 6  << endl;
+    cout << "3. 15 divided by 7 gives quotient\t = " << 15 / 7 << endl;
+    cout << "4. 15 divided by 4 has remainder\t = " << 15 / 4 << endl;
     cout << endl;
-    cout << "\t \t This is the end of my first Program" << endl;
+    cout << "\tThis is the end of my first Program" << endl;
     cout << "\t \t \t \t \t Thank you!" << endl;    
     return 0;
 }
