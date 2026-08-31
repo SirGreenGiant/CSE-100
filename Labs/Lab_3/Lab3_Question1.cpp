11a368ab1e2e8ebd3c612dcac6c6553c27316b62
SirGreenGiant <brendans0901@gmail.com>
2026-08-31 14:23:03 -0600

    Update Lab3_Question1.cpp

--- Labs/Lab_3/Lab3_Question1.cpp
+++ Labs/Lab_3/Lab3_Question1.cpp
@@ -36,7 +36,6 @@ switch (choice)
             {cout << "Incorrect Value!" << endl;}
             else {
         area = 0.5 * 3.14 * radius * radius;
-        cout << fixed << showpoint << setprecision(4);
         cout << "Area of the Circle is " << area << endl;}
         break;
     case 3 : 
