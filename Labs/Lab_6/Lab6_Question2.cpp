e32adc5d036012d0efa0ca80372e25de1c5bd4b4
SirGreenGiant <brendans0901@gmail.com>
2026-09-24 13:33:25 -0600

    L6Q2 Upload
    This is the framework and pseudo code for L6Q2. I also renamed Question 1s pseudo and made both into .txt files which I didnt know I could do.

---
+++ Labs/Lab_6/Lab6_Question2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip>
+
+// Functions 
+double getDeposit(int month);
+double getWithdrawal(int month, double balance);
+double calculateMonthlyInterest(double annualRate, double startingBalance, double endingBalance);
+
+
+int main(){
+
+// Variables
+int month;
+double balance;
+double annualRate;
+double startingBalance;
+double endingBalance;
+double averageBalance;
+
+std:: cout << std::fixed << std::setprecision(2);
+
+
+
+
+return 0;
+}
+
+double getDeposit(int month){}
+double getWithdrawal(int month, double balance){}
+double calculateMonthlyInterest(double annualRate, double startingBalance, double endingBalance){}
