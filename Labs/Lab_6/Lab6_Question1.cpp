21294a258283ac16b22f175bb6c952ee9084da2d
SirGreenGiant <brendans0901@gmail.com>
2026-09-24 12:36:36 -0600

    L6Q1 v9
    Final Formatting Adjustments
    
    100% on Gradescope!!!
    
    So many hours put into this lol

--- Labs/Lab_6/Lab6_Question1.cpp
+++ Labs/Lab_6/Lab6_Question1.cpp
@@ -8,10 +8,10 @@ int monthCheck(std::string month); // Contains the months with their spelling an
 std::string suffixFunction_max(int max_snow_day);
 std::string suffixFunction_min(int min_snow_day);
 
-
-//Global Variables 
+// Global Arrays 
 std::string monthNamesArray[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 int monthDaysArray[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
 
 int main (){
     // Variables 
@@ -19,16 +19,16 @@ int main (){
     int monthIndex;
     int max_days;
     int attempt;
-    
+    int prevDate = 0;
+    int date;
+    double snowfall;
+    int dayIndex;
+
     // Arrays
     int dates[7];
     double snowInches[7];
     int dateRange[7];
 
-   
-
-
-    
 // ! Month Input and Validation
     for (attempt = 1; attempt <= 3; attempt++){
         std::cout << "Enter the month name: ";
@@ -57,11 +57,6 @@ int main (){
         return 1;
     }
 
-    int prevDate = 0;
-    int date;
-    double snowfall;
-    int dayIndex;
-    
 // ! Daily Data Input and Validation
     for (int i = 0; i < 7; i++){
         attempt = 1;
@@ -106,10 +101,11 @@ int main (){
 
 // ! Calculations
     // Retrieving Highest Data 
-        double max_snow;
-        int max_snow_day;
-        max_snow = 0;
-        max_snow_day = 0;
+    double max_snow;
+    int max_snow_day;
+    max_snow = 0;
+    max_snow_day = 0;
+
     for (int i = 0 ; i < 7; i++){
         if (snowInches[i] > max_snow)
             {max_snow = snowInches[i];
@@ -117,10 +113,11 @@ int main (){
     }
 
     // Retrieving Lowest Data 
-        double min_snow; 
-        int min_snow_day;
-        min_snow = snowInches[0];
-        min_snow_day = dates[0];
+    double min_snow; 
+    int min_snow_day;
+    min_snow = snowInches[0];
+    min_snow_day = dates[0];
+
     for (int i = 0 ; i < 7; i++){
         if (snowInches[i] < min_snow)
             {min_snow = snowInches[i];
@@ -130,26 +127,28 @@ int main (){
     // Average 
     double averageSnow;
     double totalSnow = 0;
+
     for (int i = 0 ; i < 7; i++){
         totalSnow += snowInches[i];
     }
     averageSnow = totalSnow / 7;
 
+    // Fixing Output Precision
     std::cout << std::fixed << std::setprecision(2) << snowInches[7];
-   std::cout << std::fixed << std::setprecision(2) << snowfall;
-   std::cout << std::fixed << std::setprecision(2) << max_snow;
-   std::cout << std::fixed << std::setprecision(2) << min_snow;
-   std::cout << std::fixed << std::setprecision(2) << averageSnow;
-   std::cout << std::fixed << std::setprecision(2) << totalSnow;
+    std::cout << std::fixed << std::setprecision(2) << snowfall;
+    std::cout << std::fixed << std::setprecision(2) << max_snow;
+    std::cout << std::fixed << std::setprecision(2) << min_snow;
+    std::cout << std::fixed << std::setprecision(2) << averageSnow;
+    std::cout << std::fixed << std::setprecision(2) << totalSnow;
 
 
 // ! FINAL REPORT 
     std::cout << '\n' << "Snow report " << month << " " << dates[0] << " - " << dates[6]<< '\n';
     std::cout << "============\n";
     std::cout << "Date\tSnow Fall\n";
-    for (int i = 0; i < 7; i++){
-        std::cout << "  " << dates[i] << "\t" << snowInches[i] << '\n';
-    }
+        for (int i = 0; i < 7; i++){
+            std::cout << "  " << dates[i] << "\t" << snowInches[i] << '\n';
+        }
     std::cout << "============\n";
     std::cout << "Highest snow fall is " << max_snow << " on " << max_snow_day << suffixFunction_max(max_snow_day) << '\n';
     std::cout << "Lowest snow fall is " << min_snow << " on " << min_snow_day << suffixFunction_min(min_snow_day) << '\n';
@@ -171,33 +170,25 @@ int monthCheck(std::string month){
 // Suffix for numbers 1-31 (MAX)
 std::string suffixFunction_max(int max_snow_day){
     if (max_snow_day == 1 || max_snow_day == 21 || max_snow_day == 31){
-        return "st";
-    }
+        return "st";}
     if (max_snow_day == 2 || max_snow_day == 22){
-        return "nd";
-    }
+        return "nd";}
     if (max_snow_day == 3 || max_snow_day == 23){
-        return "rd";
-    }
+        return "rd";}
     if (max_snow_day >= 4 && max_snow_day <= 20){
-        return "th";
-    }
-        return "th";
+        return "th";}
+    return "th";
 }
 
 // Suffix for numbers 1-31 (MIN)
 std::string suffixFunction_min(int min_snow_day){
     if (min_snow_day == 1 || min_snow_day == 21 || min_snow_day == 31){
-        return "st";
-    }
+        return "st";}
     if (min_snow_day == 2 || min_snow_day == 22){
-        return "nd";
-    }
+        return "nd";}
     if (min_snow_day == 3 || min_snow_day == 23){
-        return "rd";
-    }
+        return "rd";}
     if (min_snow_day >= 4 && min_snow_day <= 20){
-        return "th";
-    }
-        return "th";
+        return "th";}
+    return "th";
 }
